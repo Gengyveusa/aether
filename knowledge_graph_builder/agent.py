6f1723ede7f218de1386b36bb63d39514fb9085b
GengyveUSA <thad@gengyveusa.com>
2025-11-20 22:41:04 -0800

    Add KnowledgeGraphBuilder cAdd KnowledgeGraphBuilder class with build_graph methodlass with build_graph method

---
+++ knowledge_graph_builder/agent.py
@@ -0,0 +1,10 @@
+class KnowledgeGraphBuilder:
+    """
+    Builds and updates a knowledge graph from extracted entities.
+    """
+
+    def build_graph(self, entities: list):
+        """
+        Placeholder method to construct or update the knowledge graph using extracted entities.
+        """
+        pass
